143c7b675fbd2e8b1244ee739862cedb7581f32f
Artem Gorbatuk <gorbatuk.am@gmail.com>
2022-12-06 05:02:32 +0100

    Read operation was created

---
+++ Api/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public readonly IServiceEmployee serviceEmployee;
+
+        public EmployeeController(IServiceEmployee serviceEmployee)
+        {
+            this.serviceEmployee = serviceEmployee;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> Get()
+        {
+            var employees = await serviceEmployee.GetAsync();
+
+            return Ok(employees);
+        }
+    }
+}
---
+++ Service/ServiceEmployee.cs
@@ -0,0 +1,12 @@
+﻿namespace Service
+{
+    public class ServiceEmployee : IServiceEmployee
+    {
+        public Task<IEnumerable<Employee>> GetAsync()
+        {
+            var result = Enumerable.Range(1, 5).Select(p => new Employee { Id = p, Name = $"Employee {p}" });
+
+            return Task.FromResult(result);
+        }
+    }
+}
